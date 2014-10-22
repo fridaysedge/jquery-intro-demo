6e9553414814a2227767599a523b4aefcc2e366b
Chris Sumption <fridaysedge@gmail.com>
2014-10-22 12:15:06 -0700

    Changes at the end of class

--- js/app.js
+++ js/app.js
@@ -4,15 +4,44 @@
 
 "use strict";
 
-jQuery(document).ready(function(){
-    jQuery('p a[href!="#top"]').attr('target', '_blank');
-    jQuery('section').hide().fadeIn(1000);
+$(document).ready(function(){
+    $('p a[href!="#top"]').attr('target', '_blank');
+    $('section').hide().fadeIn(1000);
 
-    jQuery('nav a, p a[href="#top"]').click(function(eventObject){
+    $('nav a, p a[href="#top"]').click(function(eventObject){
         console.log(this.hash);
         var targetElement = jQuery(this.hash);
-        jQuery('html,body').animate({scrollTop: targetElement.offset().top}, 700);
+        $('html,body').animate({
+            scrollTop: targetElement.offset().top - navHeight
+        }, 700);
 
         eventObject.preventDefault();
     });
+
+    var nav = $('nav');
+    var navTop = nav.offset().top;
+    var navHeight = nav.outerHeight();
+    var navPlaceholder = $('.nav-placeholder');
+    navPlaceholder.height(navHeight);
+
+    $(window).scroll(function(){
+        var scrollPos = $(this).scrollTop();
+        console.log(scrollPos);
+        if(scrollPos > navTop) {
+            nav.addClass('nav-fixed');
+            navPlaceholder.show();
+        }else{
+            nav.removeClass('nav-fixed');
+            navPlaceholder.hide();
+        }
+    });
+
+    $('#exit-button').click(function(){
+       $('#confirm-exit-modal').modal();
+    });
+
+    $('#confirm-exit-button').click(function(){
+        window.location = 'http://chrissumption.com/';
+    });
+
 });
