83a58848b5c52806875970556b3d7bc5b5e7a177
Chris Sumption <fridaysedge@gmail.com>
2014-10-22 11:26:38 -0700

    code at the break point of class

--- js/app.js
+++ js/app.js
@@ -4,3 +4,15 @@
 
 "use strict";
 
+jQuery(document).ready(function(){
+    jQuery('p a[href!="#top"]').attr('target', '_blank');
+    jQuery('section').hide().fadeIn(1000);
+
+    jQuery('nav a, p a[href="#top"]').click(function(eventObject){
+        console.log(this.hash);
+        var targetElement = jQuery(this.hash);
+        jQuery('html,body').animate({scrollTop: targetElement.offset().top}, 700);
+
+        eventObject.preventDefault();
+    });
+});
